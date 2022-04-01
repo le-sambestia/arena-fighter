79817028fc66ec8a6bb38f0fd17db9c0543d9013
le_sambestia <515985@chrome.escg.ac.uk>
2022-04-01 07:58:07 +0100

    updated health and stamina sliders

--- Assets/Scripts/UI.cs
+++ Assets/Scripts/UI.cs
@@ -2,11 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UI : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI healthText = default;
     [SerializeField] private TextMeshProUGUI staminaText = default;
+    [SerializeField] private Slider healthSlider = default;
+    [SerializeField] private Slider staminaSlider = default;
 
     private void OnEnable()
     {
@@ -23,13 +26,16 @@ public class UI : MonoBehaviour
     private void Start()
     {
         UpdateHealth(100);
+        UpdateStamina(100);
     }
     private void UpdateHealth(float currentHealth)
     {
+        healthSlider.value = currentHealth;
         healthText.text = currentHealth.ToString("00");
     }
     private void UpdateStamina(float currentStamina)
     {
+        staminaSlider.value = currentStamina;
         staminaText.text = currentStamina.ToString("00");
     }
 }
